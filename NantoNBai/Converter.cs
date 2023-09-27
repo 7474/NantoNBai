63e7593eebc099eaae1bf7bcb7879030d9047be3
koudenpa <koudenpa@hatena.ne.jp>
2023-09-28 03:46:51 +0900

    がぞうにする

---
+++ NantoNBai/Converter.cs
@@ -0,0 +1,22 @@
+﻿using Spire.Presentation;
+using System.IO;
+
+namespace NantoNBai
+{
+    public class Converter
+    {
+        public Stream ConvertFromPptx(Stream pptx)
+        {
+            var presentation = new Presentation();
+            presentation.LoadFromStream(pptx, FileFormat.Pptx2013);
+            var slide = presentation.Slides[0];
+            var svg = slide.SaveToSVG();
+
+            var ms = new MemoryStream();
+            ms.Write(svg);
+            ms.Position = 0;
+
+            return ms;
+        }
+    }
+}
--- NantoNBaiFunction/NantoNBaiFunction.cs
+++ NantoNBaiFunction/NantoNBaiFunction.cs
@@ -17,11 +17,13 @@ namespace NantoNBaiFunction
     {
         private readonly ILogger<NantoNBaiFunction> _logger;
         private readonly INantoNBaiService _nantoNBaiService;
+        private readonly Converter _converter;
 
         public NantoNBaiFunction(ILogger<NantoNBaiFunction> log)
         {
             _logger = log;
             _nantoNBaiService = new NantoNBaiShapeCrawler();
+            _converter = new Converter();
         }
 
         [FunctionName("Generate")]
@@ -30,6 +32,7 @@ namespace NantoNBaiFunction
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiParameter(name: "from", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **From** parameter")]
         [OpenApiParameter(name: "to", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **To** parameter")]
+        [OpenApiParameter(name: "format", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Format** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Generate(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
@@ -39,8 +42,20 @@ namespace NantoNBaiFunction
             string name = req.Query["name"];
             var from = double.Parse(req.Query["from"]);
             var to = double.Parse(req.Query["to"]);
+            var format = req.Query["format"];
 
             var ms = _nantoNBaiService.Generate(name, from, to, "application/vnd.openxmlformats-officedocument.presentationml.presentation");
+
+            if (format == "svg")
+            {
+                var svg = _converter.ConvertFromPptx(ms);
+                ms.Dispose();
+
+                return new FileStreamResult(svg, "image/svg+xml")
+                {
+                    FileDownloadName = $"{name}.svg"
+                };
+            }
 
             return new FileStreamResult(ms, "application/octet-stream")
             {
