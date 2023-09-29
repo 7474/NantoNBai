18700d063443bc2c552d23a345394d49250309cf
koudenpa <koudenpa@hatena.ne.jp>
2023-09-29 19:31:33 +0900

    pptx以外は添付しない。

--- NantoNBaiFunction/NantoNBaiFunction.cs
+++ NantoNBaiFunction/NantoNBaiFunction.cs
@@ -26,18 +26,18 @@ namespace NantoNBaiFunction
             _converter = new Converter();
         }
 
-        [FunctionName("Generate")]
-        [OpenApiOperation(operationId: "Generate", tags: new[] { "name" })]
-        //[OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [FunctionName(nameof(Generate))]
+        [OpenApiOperation("Generate")]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiParameter(name: "from", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **From** parameter")]
         [OpenApiParameter(name: "to", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **To** parameter")]
-        [OpenApiParameter(name: "format", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Format** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/octet-stream", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiParameter(name: "format", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The **Format** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/octet-stream", bodyType: typeof(byte[]))]
         public async Task<IActionResult> Generate(
             ExecutionContext executionContext,
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Generate.{format:alpha}")] HttpRequest req,
-            string format)
+            string format
+        )
         {
             _logger.LogInformation($"C# HTTP trigger function processed a request. path: {req.Path}, query: {req.QueryString}");
 
@@ -49,13 +49,13 @@ namespace NantoNBaiFunction
 
             if (format == "svg")
             {
-                var svg = _converter.ConvertFromPptx(ms);
+                var imageFileStream = _converter.ConvertFromPptx(ms);
                 ms.Dispose();
+                // XXX MemoryStream が返ってくることは知ってるけれどなー。byte[]返却でいいかも。
+                using var ms2 = new MemoryStream();
+                await imageFileStream.CopyToAsync(ms2);
 
-                return new FileStreamResult(svg, "image/svg+xml")
-                {
-                    FileDownloadName = $"{name}.svg"
-                };
+                return new FileContentResult(ms2.ToArray(), "image/svg+xml");
             }
 
             return new FileStreamResult(ms, "application/octet-stream")
