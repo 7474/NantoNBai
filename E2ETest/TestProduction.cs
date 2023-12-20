ca9885bb1cc8e0328a4e39227b8f61bea9480f7a
koudenpa <koudenpa@hatena.ne.jp>
2023-12-20 20:41:26 +0900

    Fix

--- E2ETest/TestProduction.cs
+++ E2ETest/TestProduction.cs
@@ -10,12 +10,12 @@ namespace E2ETest
         public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
         {
             // https://n-bai.koudenpa.dev/api/Viewer?name=ポート番号&from=80&to=443
-            await Page.GotoAsync("https://n-bai.koudenpa.dev/api/Generate.png?name=ポート番号&from=80&to=443");
+            var res = await Page.GotoAsync("https://n-bai.koudenpa.dev/api/Generate.png?name=ポート番号&from=80&to=443");
 
             var screenshot = await Page.ScreenshotAsync();
-            var expectedImage = SixLabors.ImageSharp.Image.Load("expectedImage.png");
+            using var expectedImage = SixLabors.ImageSharp.Image.Load("expect.png");
+            using var actualImage = SixLabors.ImageSharp.Image.Load(await res.BodyAsync());
 
-            using var actualImage = SixLabors.ImageSharp.Image.Load(screenshot);
             actualImage.Mutate(x => x.Resize(expectedImage.Width, expectedImage.Height));
             var calcDiff = ImageSharpCompare.CalcDiff(actualImage, expectedImage);
 
